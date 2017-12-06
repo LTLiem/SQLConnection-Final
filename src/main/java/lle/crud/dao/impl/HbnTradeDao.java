d52974f880aa9c08e651b7b33cfefc147be48a83
ngu.nguyen@upskills.com <NGU.NGUYEN@upskills.com>
2017-12-06 18:03:01 +0700

    * Update jdbc url
    - To fix error "Communication link failure"

--- src/main/java/lle/crud/dao/impl/HbnTradeDao.java
+++ src/main/java/lle/crud/dao/impl/HbnTradeDao.java
@@ -3,6 +3,7 @@ package lle.crud.dao.impl;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map.Entry;
+import java.util.stream.Collectors;
 import java.util.Set;
 
 import org.hibernate.query.Query;
@@ -15,28 +16,30 @@ import lle.crud.model.Trade;
 public class HbnTradeDao extends AbstractHbnDao<Trade> implements TradeDao {
 
 	/**
+	 * @author LuanNgu
 	 * @param criteria
-	 * @return
+	 * @return list of trades
 	 */
 	public List<Trade> getTradeByCriteria(HashMap<String, String> criteria) {
 		List<Trade> trades = null;
-		Set<Entry<String,String>> set = criteria.entrySet();
+		Set<Entry<String, String>> set = criteria.entrySet();
 		StringBuilder sb = new StringBuilder();
-		int i=0;
-		
+		int i = 0;
+
 		for (Entry<String, String> entry : set) {
 			String con = "";
-			if (i++ == 0)
-				con = String.format(" %s = :%s", entry.getKey(), entry.getKey());
-			else 
+			if (i++ > 0)
 				con = String.format("AND %s = :%s", entry.getKey(), entry.getKey());
+			else
+				con = String.format(" %s = :%s", entry.getKey(), entry.getKey());
 			sb.append(con);
 		}
-		Query query = getSession().createQuery("from Trade where "+sb.toString());
-		
+		Query query = getSession().createQuery("from Trade where " + sb.toString());
+
 		for (Entry<String, String> entry : set) {
 			query.setParameter(entry.getKey(), entry.getValue());
 		}
+
 		trades = query.getResultList();
 		return trades;
 
